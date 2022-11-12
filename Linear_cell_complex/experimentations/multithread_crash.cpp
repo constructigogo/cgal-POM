eb19997dc9a37d5141aebf551423ef2a1788ae32
Tom Clabault <TomClabault@github.com>
2022-11-12 16:06:13 +0100

    Updated multithread_crash.cpp with a new construct_lcc_crash function

--- Linear_cell_complex/experimentations/multithread_crash.cpp
+++ Linear_cell_complex/experimentations/multithread_crash.cpp
@@ -1,11 +1,15 @@
+#define CGAL_NO_ASSERTIONS
+
 #include <CGAL/Linear_cell_complex_for_combinatorial_map.h>
 #include <CGAL/Generalized_map.h>
 #include <CGAL/Linear_cell_complex_constructors.h>
+#include <CGAL/Polyhedron_3_to_lcc.h>
 
+#include <functional>//for std::function
 #include <thread>
 
 //Nombre de threads qui vont parcourir la LCC en parallèle
-#define NB_THREADS 10
+#define NB_THREADS 2
 
 //Combien de fois on va répéter l'itération sur 
 //la LCC. Ce paramètre sert uniquement à avoir
@@ -16,7 +20,7 @@
 //Si on répète 100 fois les itérations, on a 
 //beaucoup plus de chance de faire crasher 
 //le programme
-#define ITERATIONS 10
+#define ITERATIONS 1
 
 using namespace CGAL;
 
@@ -91,43 +95,37 @@ void vertices_darts_to_file(LCC& lcc, int threadId) {
     }
 }
 
-int main()
-{
+void import_bunny_into_lcc(LCC& lcc, int thread_id) {
+    std::ifstream inputFile("../data/bunny00.off");
+
+    CGAL::import_from_polyhedron_3_flux<LCC>(lcc, inputFile);
+}
+
+void iterator_crash() {
     //Tableau qui va stocker tous les handles des threads qu'on va créer
     std::array<std::thread, NB_THREADS> threads;
 
     LCC lcc;
 
-    //Création de deux cubes pour remplir la LCC
-    Dart_descriptor d1=
-    lcc.make_hexahedron(Point(0,0,0), Point(5,0,0),
-                            Point(5,5,0), Point(0,5,0),
-                            Point(0,5,4), Point(0,0,4),
-                            Point(5,0,4), Point(5,5,4));
-    Dart_descriptor d2=
-    lcc.make_hexahedron(Point(5,0,0), Point(10,0,0),
-                            Point(10,5,0), Point(5,5,0),
-                            Point(5,5,4), Point(5,0,4),
-                            Point(10,0,4), Point(10,5,4));
-
+    std::ifstream inputFile("../data/bunny00.off");
+    CGAL::import_from_polyhedron_3_flux<LCC>(lcc, inputFile);
 
     //En lançant deux threads qui vont itérer sur les même darts,
-    //on va avoir des problèmes de concurrence sur la gestion des 
+    //on va avoir des problèmes de concurrence sur la gestion des
     //marks en interne et ça va exploser
-
     for(int iter = 0; iter < ITERATIONS; iter++)
     {
         for(int i = 0; i < NB_THREADS; i++)
         {
-            threads[i] = std::thread(display_vertices_darts, std::ref(lcc), i + 1);
+            threads[i] = std::thread( display_vertices_darts, std::ref(lcc), i + 1);
 
             //Si on décommente cette ligne, les deux
-            //threads ne vont pas s'exécuter en parallèle 
-            //(puisque le main thread va attendre la fin 
-            //du thread 1 avant de lancer le deuxième thread) 
+            //threads ne vont pas s'exécuter en parallèle
+            //(puisque le main thread va attendre la fin
+            //du thread 1 avant de lancer le deuxième thread)
             //et donc on ne va pas avoir de race condition
 
-            //thread[i].join();
+            //threads[i].join();
         }
 
         //On join tous les threads à la fin de chaque itération
@@ -138,9 +136,9 @@ int main()
         }
     }
 
-    //On va attendre la fin de tous les 
-    //threads pour pas que le main thread 
-    //finisse son exécution et donc destroy 
+    //On va attendre la fin de tous les
+    //threads pour pas que le main thread
+    //finisse son exécution et donc destroy
     //la LCC sur laquelle les threads sont
     //en train d'itérer
     for(int i = 0; i < NB_THREADS; i++)
@@ -148,4 +146,39 @@ int main()
         if(threads[i].joinable())
             threads[i].join();
     }
+}
+
+void construct_lcc_crash() {
+    LCC lcc;
+
+    std::array<std::thread, NB_THREADS> threads;
+
+    for(int iter = 0; iter < ITERATIONS; iter++)
+    {
+        for(int i = 0; i < NB_THREADS; i++)
+        {
+            threads[i] = std::thread(import_bunny_into_lcc, std::ref(lcc), i + 1);
+
+            //threads[i].join();
+        }
+
+        //On join tous les threads à la fin de chaque itération
+        for(int i = 0; i < NB_THREADS; i++)
+        {
+            if(threads[i].joinable())
+                threads[i].join();
+        }
+    }
+
+    for(int i = 0; i < NB_THREADS; i++)
+    {
+        if(threads[i].joinable())
+            threads[i].join();
+    }
+}
+
+int main()
+{
+    //iterator_crash();
+    construct_lcc_crash();
 }
